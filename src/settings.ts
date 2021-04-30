92256755e605db9605e5f783e5acb158af68a18e
e111one <eleventeen.onety.one@gmail.com>
2021-04-30 17:36:20 +0300

    Fix drag & drop, add form factory

--- src/settings.ts
+++ src/settings.ts
@@ -4,32 +4,45 @@ export type Settings = {
 
 }
 
-export class SettingsForm extends FormApplication<FormApplication.Options, FormApplication.Data<Settings>, Settings> {
+export class SettingsForm {
 
-    protected async _updateObject(event: Event, formData?: object) {
-        throw new Error("Method not implemented.");
-    }
+    static factory(game: Game, config: Config): ConstructorOf<FormApplication<FormApplication.Options, FormApplication.Data<Settings>, Settings>> {
+        return class Form extends FormApplication<FormApplication.Options, FormApplication.Data<Settings>, Settings> {
 
-    protected _onDrop(event: DragEvent) {
-        console.log(event.dataTransfer.getData('text/plain'));
-        super._onDrop(event);
-    }
+            constructor(settings?: Settings, options?: Partial<FormApplication.Options>) {
+                super(settings, options)
+                console.log(game)
+                console.log(config)
+                console.log(settings)
+                console.log(options)
+            }
 
-    static get defaultOptions(): FormApplication.Options {
-        return {
-            ... super.defaultOptions,
-            ... {
-                title: "Charactermancer Configuration",
-                id: Config.name,
-                template: 'modules/charactermancer/templates/settings.html',
-                closeOnSubmit: true,
-                submitOnChange: true,
-                submitOnClose: true,
-                popOut: true,
-                width: 600,
-                height: 'auto',
-                resizable: true,
-                dragDrop: [{ dropSelector: ".drop" }]
+            protected async _updateObject(event: Event, formData?: object) {
+                throw new Error("Method not implemented.");
+            }
+
+            protected _onDrop(event: DragEvent) {
+                console.log(event.dataTransfer.getData('text/plain'));
+                super._onDrop(event);
+            }
+
+            static get defaultOptions(): FormApplication.Options {
+                return {
+                    ... super.defaultOptions,
+                    ... {
+                        title: "Charactermancer Configuration",
+                        id: Config.name,
+                        template: 'modules/charactermancer/templates/settings.html',
+                        closeOnSubmit: true,
+                        submitOnChange: true,
+                        submitOnClose: true,
+                        popOut: true,
+                        width: 600,
+                        height: 'auto',
+                        resizable: true,
+                        dragDrop: [{ dragSelector: ".drag" }]
+                    }
+                }
             }
         }
     }
@@ -39,7 +52,7 @@ export class SettingsForm extends FormApplication<FormApplication.Options, FormA
             name: "Charactermancer Configuration",
             label: "Magic lies ahead",
             icon: "fas fa-bars",
-            type: SettingsForm,
+            type: SettingsForm.factory(game, config),
             restricted: true // Restrict this submenu to gamemaster only?
         })
     }
