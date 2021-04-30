62ad951492a598e07c0038037469b58a049e1579
PyroPonya <43729142+PyroPonya@users.noreply.github.com>
2021-04-30 22:49:39 +0300

    Update progression.ts

--- src/progression.ts
+++ src/progression.ts
@@ -1,11 +1,20 @@
-type Progression = {
+type RaceProgression = {
+    inits: {
+        features: [Races],
+        /*@TODO: resistances and proficiencies based on race features*/
+    }
+}
+
+type ClassProgression = {
     class: Item | string,
     hitDice: HitDice
     inits: {
         hp: HpFormula
         features: [Feature],
         proficiencies: [Proficience],
-        equipment: [Equipment]
+        /*equipment: [Equipment],*/
+        spellcasting: [Spellcasting],
+        classStarterBackpack: [ClassStarterBackpack],
     },
     milestones: [Milestone]
 }
@@ -14,6 +23,41 @@ type HitDice = {
     n: number
 }
 
+type HpFormula = {
+    init: number,
+    modifier: string
+}
+
+type Milestone = {
+    level: number,
+    proficiencyBonus: number,
+    /*features: [Feature]*/
+}
+
+type Spellcasting = {
+    spellcastingAbility: boolean,
+    spellcastingType: Item | string,
+    hasSpells: string,
+    /*hasSpellslots: number[]*/
+}
+
+type ClassStarterBackpack = {
+    starterGear: [StarterGear],
+    starterCoins: [StarterCoins]
+}
+
+type StarterGear = {
+    Armor: [Armor],
+    Weapon: [Weapon],
+    /*@TODO: background items?*/
+}
+
+type StarterCoins = {
+    coins: number | null
+}
+
+type Races = Item | string
+type Feature = Item | string
 type Armor = string
 type Weapon = string
 type Tool = string
@@ -22,23 +66,10 @@ type Skill = string
 type Other = Item | string
 type Proficience = Armor | Weapon | Tool | SavingThrow | Skill | Other
 
-type HpFormula = {
-    init: number,
-    modifier: string
-}
+/*type Equipment = Item*/
 
-type Feature = Item | string
-
-type Equipment = Item
-
-type Milestone = {
-    level: number,
-    proficiencyBonus: number,
-    features: [Feature]
-}
-
-class Prog {
+/*class Prog {
 
     class: Item
 
-}
+}*/
